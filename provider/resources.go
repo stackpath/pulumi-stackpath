053e3c98cf69b5478d6421c529f5e9361fa7daf7
Nebojša Koturović <nebojsa.koturovic@stackpath.com>
2024-01-02 15:43:32 +0100

    Pull updates from the original template
    Also: Add various tweaks to the project

--- provider/resources.go
+++ provider/resources.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2023, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,18 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package provider
+package stackpath
 
 import (
 	"fmt"
-	"path/filepath"
+	"path"
+
+	// Allow embedding bridge-metadata.json in the provider.
+	_ "embed"
 
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+
+	// Replace this provider with the provider you are bridging.
+	stackpath "github.com/stackpath/terraform-provider-stackpath/stackpath"
+
 	"github.com/stackpath/pulumi-stackpath/provider/pkg/version"
-	"github.com/stackpath/terraform-provider-stackpath/stackpath"
 )
 
 // all of the token components used below.
@@ -32,6 +36,7 @@ const (
 	// registries for nodejs and python:
 	mainPkg = "stackpath"
 	// modules:
+	// mainMod = "index" // the stackpath module
 	computeMod = "compute"
 	storageMod = "storage"
 )
@@ -40,22 +45,23 @@ const (
 // It should validate that the provider can be configured, and provide actionable errors in the case
 // it cannot be. Configuration variables can be read from `vars` using the `stringValue` function -
 // for example `stringValue(vars, "accessKey")`.
-func preConfigureCallback(vars resource.PropertyMap, c shim.ResourceConfig) error {
-	return nil
-}
+// func preConfigureCallback(resource.PropertyMap, shim.ResourceConfig) error {
+// 	return nil
+// }
+
+//go:embed cmd/pulumi-resource-stackpath/bridge-metadata.json
+var metadata []byte
 
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
-	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(stackpath.Provider())
-
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
-		P:    p,
+		// Instantiate the Terraform provider
+		P:    shimv2.NewProvider(stackpath.Provider()),
 		Name: "stackpath",
 		// DisplayName is a way to be able to change the casing of the provider
 		// name when being displayed on the Pulumi registry
-		DisplayName: "",
+		DisplayName: "StackPath",
 		// The default publisher for all packages is Pulumi.
 		// Change this to your personal name (or a company name) that you
 		// would like to be shown in the Pulumi Registry if this package is published
@@ -70,7 +76,7 @@ func Provider() tfbridge.ProviderInfo {
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "",
+		PluginDownloadURL: "https://github.com/stackpath/pulumi-stackpath/releases/download/v${VERSION}",
 		Description:       "A Pulumi package for creating and managing stackpath cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
@@ -81,18 +87,42 @@ func Provider() tfbridge.ProviderInfo {
 		Repository: "https://github.com/stackpath/pulumi-stackpath",
 		// The GitHub Org for the provider - defaults to `terraform-providers`. Note that this
 		// should match the TF provider module's require directive, not any replace directives.
-		GitHubOrg:            "stackpath",
-		Config:               map[string]*tfbridge.SchemaInfo{},
-		PreConfigureCallback: preConfigureCallback,
+		GitHubOrg:    "stackpath",
+		MetadataInfo: tfbridge.NewProviderMetadata(metadata),
+		Config:       map[string]*tfbridge.SchemaInfo{
+			// Add any required configuration here, or remove the example below if
+			// no additional points are required.
+			// "region": {
+			// 	Type: tfbridge.MakeType("region", "Region"),
+			// 	Default: &tfbridge.DefaultInfo{
+			// 		EnvVars: []string{"AWS_REGION", "AWS_DEFAULT_REGION"},
+			// 	},
+			// },
+		},
+		// PreConfigureCallback: preConfigureCallback,
 		Resources: map[string]*tfbridge.ResourceInfo{
 			// Compute
-			"stackpath_compute_network_policy": {Tok: tfbridge.MakeResource(mainPkg, computeMod, "NetworkPolicy")},
-			"stackpath_compute_vpc_network":    {Tok: tfbridge.MakeResource(mainPkg, computeMod, "Network")},
-			"stackpath_compute_vpc_route":      {Tok: tfbridge.MakeResource(mainPkg, computeMod, "NetworkRoute")},
-			"stackpath_compute_workload":       {Tok: tfbridge.MakeResource(mainPkg, computeMod, "Workload")},
+			"stackpath_compute_network_policy":     {Tok: tfbridge.MakeResource(mainPkg, computeMod, "NetworkPolicy")},
+			"stackpath_compute_vpc_network":        {Tok: tfbridge.MakeResource(mainPkg, computeMod, "Network")},
+			"stackpath_compute_vpc_network_subnet": {Tok: tfbridge.MakeResource(mainPkg, computeMod, "NetworkSubnet")},
+			"stackpath_compute_vpc_route":          {Tok: tfbridge.MakeResource(mainPkg, computeMod, "NetworkRoute")},
+			"stackpath_compute_workload":           {Tok: tfbridge.MakeResource(mainPkg, computeMod, "Workload")},
 
 			// Storage
 			"stackpath_object_storage_bucket": {Tok: tfbridge.MakeResource(mainPkg, storageMod, "Bucket")},
+
+			// Map each resource in the Terraform provider to a Pulumi type. Two examples
+			// are below - the single line form is the common case. The multi-line form is
+			// needed only if you wish to override types or other default options.
+			//
+			// "aws_iam_role": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "IamRole")}
+			//
+			// "aws_acm_certificate": {
+			// 	Tok: tfbridge.MakeResource(mainPkg, mainMod, "Certificate"),
+			// 	Fields: map[string]*tfbridge.SchemaInfo{
+			// 		"tags": {Type: tfbridge.MakeType(mainPkg, "Tags")},
+			// 	},
+			// },
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi function. An example
@@ -100,6 +130,7 @@ func Provider() tfbridge.ProviderInfo {
 			// "aws_ami": {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAmi")},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@stackpath/pulumi-stackpath",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
@@ -114,13 +145,14 @@ func Provider() tfbridge.ProviderInfo {
 			//Overlay: &tfbridge.OverlayInfo{},
 		},
 		Python: &tfbridge.PythonInfo{
+			PackageName: "pulumi_stackpath",
 			// List any Python dependencies and their version ranges
 			Requires: map[string]string{
 				"pulumi": ">=3.0.0,<4.0.0",
 			},
 		},
 		Golang: &tfbridge.GolangInfo{
-			ImportBasePath: filepath.Join(
+			ImportBasePath: path.Join(
 				fmt.Sprintf("github.com/stackpath/pulumi-%[1]s/sdk/", mainPkg),
 				tfbridge.GetModuleMajorVersion(version.Version),
 				"go",
@@ -129,12 +161,20 @@ func Provider() tfbridge.ProviderInfo {
 			GenerateResourceContainerTypes: true,
 		},
 		CSharp: &tfbridge.CSharpInfo{
+			RootNamespace: "Stackpath",
 			PackageReferences: map[string]string{
 				"Pulumi": "3.*",
 			},
 		},
 	}
 
+	// These are new API's that you may opt to use to automatically compute resource
+	// tokens, and apply auto aliasing for full backwards compatibility.  For more
+	// information, please reference:
+	// https://pkg.go.dev/github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge#ProviderInfo.ComputeTokens
+	// prov.MustComputeTokens(tokens.SingleModule("stackpath_", mainMod,
+  // tokens.MakeStandard(mainPkg)))
+	// prov.MustApplyAutoAliases()
 	prov.SetAutonaming(255, "-")
 
 	return prov
